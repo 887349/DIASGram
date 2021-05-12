36cdc41e9986e022d3dd420b2c19685e6710dffe
giov-coldebella <886870@stud.unive.it>
2021-05-12 10:40:18 +0200

    comment delete

--- tensor.cpp
+++ tensor.cpp
@@ -191,7 +191,7 @@ int Tensor::depth()const {
 }
     
 
-/* ADD THROW ERROR
+
 float Tensor::getMin(int k)const {
     float min = 0;
     if (k<d && k>=0) {
@@ -223,7 +223,7 @@ float Tensor::getMax(int k)const {
     }
     return max;
 }
-*/
+
 
 void Tensor::showSize()const {
     std::cout << r << " x " << c << " x " << d <<std::endl;
